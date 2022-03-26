eeec2618096541cf3d144fda165d44df7f3dfab0
e20c2007-sm <86354855+e20c2007-sm@users.noreply.github.com>
2022-03-27 03:49:53 +0900

    c

--- vp/main/index.js
+++ vp/main/index.js
@@ -2,6 +2,12 @@ setTimeout(()=>{
     $(".center-text").fadeIn(300);
 }, 500);
 
+let vpDelay;
+if(screen.view == "pc"){
+    vpDelay = 3500;
+}else{
+    vpDelay = 1500;
+}
 setTimeout(()=>{
     $("div.no-event").removeClass();
-}, 3500);
+}, vpDelay);
